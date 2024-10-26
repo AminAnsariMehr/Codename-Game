2986af8a5561b31112edf4dfaf7dec7e34652a39
Amin Ansari Mehr <aminansarimehrgeek23@gmail.com>
2024-10-26 19:58:08 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<style src="./App.scss" lang="scss" scoped></style>
+
+<template>
+  <header class="header">
+    <div class="allUsers">
+      <div class="allUsersIcon"></div>
+      <div class="allUsersNumber">19</div>
+    </div>
+
+    <div class="hostPanel">
+      <div class="timePanel"></div>
+      <div class="Panel"></div>
+    </div>
+
+    <div class="user">
+      <span class="userName">Amin</span>
+      <span class="userIcon"></span>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
